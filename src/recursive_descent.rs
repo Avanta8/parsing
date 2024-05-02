b216969b29109daf2a4479d2b560ce28c09fd2bf
Harry Min <harryhmin@gmail.com>
2024-05-02 22:30:30 +0100

    refactor: use typestate for complete or incomplete tree
    refactor: use Option for parent so parent so parent of root is None

--- src/recursive_descent.rs
+++ src/recursive_descent.rs
@@ -13,41 +13,65 @@ enum Elem<'a> {
 }
 
 enum Step<'a> {
-    Terminal(Tree<'a>),
-    NonTerminal(Vec<Tree<'a>>),
+    Terminal(TreeResult<'a>),
+    NonTerminal(Vec<TreeResult<'a>>),
 }
 
-#[derive(Debug, Clone)]
-struct Tree<'a> {
-    grammar: &'a Grammar,
-    arena: Vec<Vec<Elem<'a>>>,
-    // Invariant: position must be valid
-    position: (usize, usize),    // (arena_idx, rhs_idx)
-    parent: Vec<(usize, usize)>, // [position]
+enum TreeResult<'a> {
+    Incomplete(Tree<'a, Incomplete>),
+    Complete(Tree<'a, Complete>),
 }
 
-impl<'a> Tree<'a> {
+impl<'a> TreeResult<'a> {
     fn new(grammar: &'a Grammar) -> Self {
-        Self {
+        Tree::<Incomplete> {
             grammar,
             arena: vec![vec![Elem::Unexpanded(&grammar.start)]],
             position: (0, 0),
-            parent: vec![(69, 69)], // HACK: Change parent of root to something unique
+            parent: vec![None],
+            _complete: std::marker::PhantomData,
+        }
+        .normalize()
+    }
+}
+
+#[derive(Clone)]
+struct Incomplete;
+#[derive(Clone)]
+struct Complete;
+
+#[derive(Debug, Clone)]
+struct Tree<'a, C> {
+    grammar: &'a Grammar,
+    arena: Vec<Vec<Elem<'a>>>,
+    // Invariant: position must be valid
+    position: (usize, usize),            // (arena_idx, rhs_idx)
+    parent: Vec<Option<(usize, usize)>>, // [position]. Parent of root is None
+    _complete: std::marker::PhantomData<C>,
+}
+
+impl<'a> From<Tree<'a, Incomplete>> for Tree<'a, Complete> {
+    fn from(value: Tree<'a, Incomplete>) -> Self {
+        Self {
+            grammar: value.grammar,
+            arena: value.arena,
+            position: value.position,
+            parent: value.parent,
+            _complete: std::marker::PhantomData,
         }
     }
+}
 
-    fn is_complete(&self) -> bool {
-        self.position == (69, 69)
-    }
-
-    fn normalize(&mut self) {
+impl<'a> Tree<'a, Incomplete> {
+    fn normalize(mut self) -> TreeResult<'a> {
         while self.position.1 >= self.arena[self.position.0].len() {
-            self.position = self.parent[self.position.0];
-            if self.position == (69, 69) {
-                break;
-            }
+            let Some(position) = self.parent[self.position.0] else {
+                return TreeResult::Complete(self.into());
+            };
+            self.position = position;
             self.position.1 += 1;
         }
+        TreeResult::Incomplete(self)
     }
 
     fn step(mut self, token: Option<&str>) -> Step<'a> {
@@ -58,8 +82,7 @@ impl<'a> Tree<'a> {
                     return Step::NonTerminal([].into());
                 }
                 self.position.1 += 1;
-                self.normalize();
-                Step::Terminal(self)
+                Step::Terminal(self.normalize())
             }
             Elem::Unexpanded(nt) => {
                 let mut new_trees = vec![];
@@ -77,17 +100,18 @@ impl<'a> Tree<'a> {
                         Elem::Expanded(nt, self.arena.len());
                     new_tree.position = (self.arena.len(), 0);
                     new_tree.arena.push(subtree);
-                    new_tree.parent.push(self.position);
+                    new_tree.parent.push(Some(self.position));
 
-                    new_tree.normalize();
-                    new_trees.push(new_tree);
+                    new_trees.push(new_tree.normalize());
                 }
                 Step::NonTerminal(new_trees)
             }
             Elem::Expanded(_, _) => unreachable!("Tree stepped into already expanded nonterminal"),
         }
     }
+}
 
+impl<'a> Tree<'a, Complete> {
     fn idx_to_astnodes(&self, arena_idx: usize) -> Vec<AstNode<'a>> {
         let elems = &self.arena[arena_idx];
         let children = elems
@@ -105,28 +129,34 @@ impl<'a> Tree<'a> {
 
     fn to_ast(&self) -> Ast<'a> {
         let AstNode::NonTerminal(start) = self.idx_to_astnodes(0).swap_remove(0) else {
-            panic!()
+            panic!("Root was not a NonTerminal")
         };
         start
     }
 }
 
 pub fn parse<'a>(grammar: &'a Grammar, tokens: &'a [String]) -> Option<Ast<'a>> {
-    let mut bag = VecDeque::from([(Tree::new(grammar), 0)]);
-    while let Some((tree, idx)) = bag.pop_back() {
-        dbg!(idx);
-        // dbg!((&tree, idx));
-        if idx == tokens.len() && tree.is_complete() {
-            return Some(tree.to_ast());
-        } else if tree.is_complete() {
-            continue;
-        }
+    // let mut bag = VecDeque::from([(Tree::new(grammar), 0)]);
+    let mut bag = VecDeque::from([(TreeResult::new(grammar), 0)]);
+    while let Some((tree_result, idx)) = bag.pop_back() {
+        match tree_result {
+            TreeResult::Incomplete(tree) => {
+                dbg!(idx);
+                // dbg!((&tree, idx));
 
-        match tree.step(tokens.get(idx).map(|x| x.as_str())) {
-            Step::Terminal(new_tree) => bag.push_front((new_tree, idx + 1)),
-            Step::NonTerminal(new_trees) => {
-                for new_tree in new_trees {
-                    bag.push_front((new_tree, idx));
+                match tree.step(tokens.get(idx).map(|x| x.as_str())) {
+                    Step::Terminal(tree_result) => bag.push_front((tree_result, idx + 1)),
+                    Step::NonTerminal(tree_results) => {
+                        for tree_result in tree_results {
+                            bag.push_front((tree_result, idx));
+                        }
+                    }
+                }
+            }
+
+            TreeResult::Complete(tree) => {
+                if idx == tokens.len() {
+                    return Some(tree.to_ast());
                 }
             }
         }
